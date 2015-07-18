5576bcf8b4ec6ec58ced2b10f37f7ea5f0e83db2
stephanrauh <stephan@beyondjava.de>
2015-07-18 13:48:32 +0200

    started to implement an BootsFaces AJAX example project

---
+++ AJAX/src/main/java/de/beyondjava/bootsfaces/examples/ImageGallery.java
@@ -0,0 +1,39 @@
+package de.beyondjava.bootsfaces.examples;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ViewScoped
+@ManagedBean
+public class ImageGallery {
+	private List<String> images = new ArrayList<String>(){{
+		add("http://www.11pictures.com/foto/stories/Camargue_2013_Nature/framedPreview.png");
+		add("http://www.11pictures.com/foto/stories/Camargue_2013/framedPreview.png");
+		add("http://www.11pictures.com/foto/stories/GreenCevennes/framedPreview.png");
+		add("http://www.11pictures.com/foto/stories/Alpilles_2013/framedPreview.png");
+	}};
+	
+	private List<String> imagePool = new ArrayList<String>() {{
+		add("http://www.11pictures.com/foto/stories/ValleeDeLaGrandMaison_2012/framedPreview.png");
+		add("http://www.11pictures.com/foto/stories/Clouds2/framedPreview.png");
+		add("http://www.11pictures.com/foto/stories/BretagneFog2003/framedPreview.png");
+		add("http://www.11pictures.com/foto/stories/life-in-winter/framedPreview.png");
+	}
+	};
+	
+	public List<String> getImages() {
+		return images;
+	}
+	
+	public void chooseAnotherImage(String image) {
+		System.out.println(image);
+		int index = images.indexOf(image);
+		String previousImage = images.get(index);
+		int random = (int) Math.floor(Math.random()*imagePool.size());
+		images.set(index, imagePool.get(random));
+		imagePool.set(random, previousImage);
+	}
+}
